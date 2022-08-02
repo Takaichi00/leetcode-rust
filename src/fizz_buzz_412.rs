4764d8f565b27508ab1e86bfa2fdb7c8edfd2bc9
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-08-02 20:19:57 +0900

    refactor: 412. FizzBuzz

--- src/fizz_buzz_412.rs
+++ src/fizz_buzz_412.rs
@@ -1,3 +1,4 @@
+use std::ops::Add;
 use derive_new::new;
 
 #[derive(new)]
@@ -6,20 +7,23 @@ pub struct Solution {}
 impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
         let mut result = vec![];
-
         for num in 1..=n {
-            if num % 15 == 0 {
-                result.push("FizzBuzz".to_string());
+
+            let divisible3 = num % 3 == 0;
+            let divisible5 = num % 5 == 0;
+
+            let mut result_str = "".to_string();
+
+            if divisible3 {
+                result_str += "Fizz";
             }
-            else if num % 3 == 0 {
-                result.push("Fizz".to_string());
+            if divisible5 {
+                result_str += "Buzz";
             }
-            else if num % 5 == 0 {
-                result.push("Buzz".to_string());
+            if result_str.is_empty() {
+                result_str += &num.to_string();
             }
-            else {
-                result.push(num.to_string());
-            }
+            result.push(result_str);
         }
         result
     }
