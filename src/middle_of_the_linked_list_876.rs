ba17472b97ae4ba6091e4acc13a2368b6cb8c774
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-08-27 17:53:02 +0900

    wip: 1342 最後の node をとる処理実装途中 (まだ失敗する)

--- src/middle_of_the_linked_list_876.rs
+++ src/middle_of_the_linked_list_876.rs
@@ -24,6 +24,18 @@ impl ListNode {
 ////////////// ↓ ここから実装をコピペする
 impl Solution {
     pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+
+        // println!("{:#}", head.clone().unwrap().val);
+        // println!("{:#}", head.clone().unwrap().next.unwrap().val);
+
+        let mut node5 = head.unwrap().get_last_node();
+        println!("node5: {:#?}", node5);
+        // next が None の ListNode を new して返す を 3回する
+        // → None になるまで ListNode を追って、None の ListNode を返す
+
+        // next が None の ListNode の 2つ上の ListNode 以外は pop する
+
+        // 1,2,3,4,5
         let mut node3 = ListNode::new(3);
         let mut node4 = ListNode::new(4);
         let mut node5 = ListNode::new(5);
@@ -45,11 +57,57 @@ impl ListNode {
     fn push(&mut self, next_node: ListNode) -> () {
         self.next = Some(Box::new(next_node));
     }
+
+    fn has_next(&self) -> bool {
+        return self.next.is_some()
+    }
+
+    fn has_next_2(&self) -> bool {
+
+        match self.clone().next.clone() {
+            Some(next_list_node) => {
+                match next_list_node.clone().next.clone() {
+                    Some(_) => {
+                        true
+                    },
+                    None => {
+                        false
+                    }
+                }
+            },
+            None => {
+                false
+            }
+        }
+
+        //
+        // if self.next.is_none() {
+        //     return false;
+        // }
+        //
+        // if self.next.unwrap().next.is_none() {
+        //     return false
+        // }
+        //
+        // true
+    }
+
+    fn get_last_node(&self) -> ListNode {
+
+        println!("self: {:#?}", self);
+
+        if self.has_next_2() {
+            return self.clone().next.clone().unwrap().get_last_node();
+        }
+        // 2個先が None だったら、2個先の self を返して、1個先の node の next を none にする
+        *self.next.clone().unwrap().next.clone().unwrap()
+    }
 }
 
 #[cfg(test)]
 mod test {
     use crate::middle_of_the_linked_list_876::{ListNode, Solution};
+
     #[test]
     fn test_solution1() {
         let mut node1 = ListNode::new(1);
