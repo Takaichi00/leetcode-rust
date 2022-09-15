4d4c16e1da44c97fe55fcf5a35932222330070e5
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-09-16 02:49:32 +0900

    wip: 383 ransom note

--- src/ransom_note_383.rs
+++ src/ransom_note_383.rs
@@ -6,6 +6,23 @@ pub struct Solution {}
 
 impl Solution {
     pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+
+        // magazine を1文字ずつの vec にする
+
+        let char_vec: Vec<char> = magazine.chars().collect::<Vec<_>>();
+        println!("{:#?}", char_vec);
+
+        // n! 通りの文字を作成する (3文字だったら↓)
+        // 0,1,2 / 0,2,1 / 1,0,2 / 1,2,0 / 2,0,1 / 2,1,0
+
+        for n in char_vec.len() {
+            let str = char_vec[0] + &char_vec[1] + &char_vec[2];
+        }
+
+        // ↑ の文字.contains ransam_note で答えが求まる
+
+
+
         magazine.contains(&ransom_note)
     }
 }
@@ -29,6 +46,12 @@ mod test {
     #[test]
     fn test_solution3() {
         let target = Solution::can_construct("aab".to_string(), "baa".to_string());
+        assert_eq!(target, true);
+    }
+
+    #[test]
+    fn test_solution4() {
+        let target = Solution::can_construct("cba".to_string(), "abc".to_string());
         assert_eq!(target, true);
     }
 }
